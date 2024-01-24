56819fc942a88bbcb485873dbed0473296d162aa
babafemiolatona <femiolatona@gmail.com>
2024-01-24 19:29:24 +0100

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,21 @@
 import redis
 import uuid
 from typing import Union, Callable, Optional
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator that counts how many times methods
+    of the Cache class are called
+    """
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function"""
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
@@ -12,24 +27,26 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """Get data from redis"""
         data = self._redis.get(key)
         if fn:
             return fn(data)
         else:
             return data
-        
+
     def get_str(self, key: str) -> str:
         """Convert bytes to str"""
         return self.get(key, str)
-    
+
     def get_int(self, key: str) -> int:
         """Convert bytes to int"""
         return self.get(key, int)
