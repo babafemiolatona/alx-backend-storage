643906313fb6ac2875ef1285385cf10a263f7cc6
babafemiolatona <femiolatona@gmail.com>
2024-01-24 19:50:57 +0100

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python3
-"""
-web cache and tracker
-"""
-import requests
+"""Web module"""
 import redis
+import requests
 from functools import wraps
 
-store = redis.Redis()
 
-
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
+def cache_and_track(func):
+    """
+    Decorator that takes a single method
+    and returns a single method
+    """
+    @wraps(func)
     def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
+        """Wrapper function"""
+        redis_store = redis.Redis()
+        res_key = f'result:{url}'
+        req_key = f'count:{url}'
+        result = redis_store.get(res_key)
+        if result is not None:
+            redis_store.incr(req_key)
+            return result.decode('utf-8')
+        result = func(url)
+        redis_store.setex(res_key, 10, result)
+        redis_store.incr(req_key)
+        return result
     return wrapper
 
 
-@count_url_access
+@cache_and_track
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    """
+    Gets the HTML content of a particular URL
+    and returns it
+    """
+    return requests.get(url).content.decode('utf-8')
